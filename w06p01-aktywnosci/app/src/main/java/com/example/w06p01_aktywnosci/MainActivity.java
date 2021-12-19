8f1ffb4da3c741755abf254536b5f80a37b73c3f
BartoszewskiA <bartoszewski.a@outlook.com>
2021-12-19 17:42:06 +0100

    19.12.2021

--- w06p01-aktywnosci/app/src/main/java/com/example/w06p01_aktywnosci/MainActivity.java
+++ w06p01-aktywnosci/app/src/main/java/com/example/w06p01_aktywnosci/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.w06p01_aktywnosci;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,8 +12,9 @@ import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button01, button02;
-    EditText editText01;
+    Button button01, button02, button03, button04 ,button05;
+    EditText editText01, editText02;
+    static int KOD = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +22,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         button01 = findViewById(R.id.button01);
         button02 = findViewById(R.id.button02);
+        button03 = findViewById(R.id.button03);
+        button04 = findViewById(R.id.button04);
+        button05 = findViewById(R.id.button05);
         editText01 = findViewById(R.id.editText01);
+        editText02 = findViewById(R.id.editText02);
         View.OnClickListener sluchacz = new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,11 +35,47 @@ public class MainActivity extends AppCompatActivity {
                 {
                     case R.id.button01: otworzAktywnosc(); break;
                     case R.id.button02: otworziPrzeslijDane(); break;
+                    case R.id.button03: otworziOdbierzDane(); break;
+                    case R.id.button04: otworzGoogle(); break;
+                    case R.id.button05: otworzGoogleMaps(); break;
                 }
             }
         };
         button01.setOnClickListener(sluchacz);
         button02.setOnClickListener(sluchacz);
+        button03.setOnClickListener(sluchacz);
+        button04.setOnClickListener(sluchacz);
+        button05.setOnClickListener(sluchacz);
+    }
+
+    private void otworzGoogleMaps() {
+        Uri adresGoogla = Uri.parse("geo:51.405,21.17");
+        Intent intencja = new Intent(Intent.ACTION_VIEW, adresGoogla);
+        startActivity(intencja);
+    }
+
+    private void otworzGoogle() {
+        Uri adresGoogla = Uri.parse("https://www.google.pl/");
+        Intent intencja = new Intent(Intent.ACTION_VIEW, adresGoogla);
+        startActivity(intencja);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==KOD && resultCode==RESULT_OK)
+        {
+            String s = data.getStringExtra("odp");
+            editText02.setText(s);
+        }
+
+    }
+
+    private void otworziOdbierzDane() {
+        Intent intencja = new Intent(getApplicationContext(),MainActivity4.class);
+        intencja.putExtra("pytanie","Jak się masz?");
+        startActivityForResult(intencja, KOD );
+
     }
 
     private void otworziPrzeslijDane() {
--- w06p01-aktywnosci/app/src/main/java/com/example/w06p01_aktywnosci/MainActivity4.java
+++ w06p01-aktywnosci/app/src/main/java/com/example/w06p01_aktywnosci/MainActivity4.java
@@ -2,13 +2,37 @@ package com.example.w06p01_aktywnosci;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
 
+    Button odpowiedz;
+    EditText dana;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+        odpowiedz = findViewById(R.id.button02_a4);
+        dana = findViewById(R.id.editText01_a4);
+        Bundle paczka = getIntent().getExtras();
+        String s = paczka.getString("pytanie");
+        //dana.setText(s);
+        dana.setHint(s);
+
+
+        odpowiedz.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent odp = new Intent();
+                odp.putExtra("odp",dana.getText().toString());
+                setResult(RESULT_OK, odp);
+                finish();
+            }
+        });
     }
 }
